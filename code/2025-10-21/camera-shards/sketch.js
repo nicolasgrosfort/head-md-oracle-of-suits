317b37816c8d9902ab27a236c337ffda4bacccb1
Nicolas Grosfort <grosfort.nicolas@gmail.com>
2025-10-21 14:51:04 +0200

    feat: cleaner area

--- code/2025-10-21/camera-shards/sketch.js
+++ code/2025-10-21/camera-shards/sketch.js
@@ -23,22 +23,28 @@ function draw() {
 }
 
 function createShard(x, y) {
-	const captureSize = random(10, 100);
-	const displaySize = random(100, 200);
+	const randomSize = () => random(10, 50);
+
+	const size = {
+		width: randomSize(),
+		height: randomSize(),
+	};
+
+	const scaleFactor = random(0.5, 5);
 
 	const { videoX, videoY } = video.canvasToVideoCoords(x, y);
 
-	const captureX = constrain(videoX - captureSize / 2, 0, 640 - captureSize);
-	const captureY = constrain(videoY - captureSize / 2, 0, 480 - captureSize);
+	const captureX = constrain(videoX - size.width / 2, 0, 640 - size.width);
+	const captureY = constrain(videoY - size.height / 2, 0, 480 - size.height);
 
 	const shard = new VideoShard(
 		video.getVideo(),
 		captureX,
 		captureY,
-		captureSize,
-		captureSize,
-		displaySize,
-		displaySize,
+		size.width,
+		size.height,
+		size.width * scaleFactor,
+		size.height * scaleFactor,
 		x,
 		y,
 	);
